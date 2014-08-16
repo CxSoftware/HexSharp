9ddf4f68d078d600cfb227d19abcb8794b3060d0
Juan C. Olivares <cristobal@cxsoftware.com>
2014-08-15 22:14:43 -0400

    Initial commit

---
+++ CxSoftware.HexSharp.Tests/HexConvertTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CxSoftware.HexSharp.Tests
+{
+	[TestFixture]
+	public class HexConvertTest
+	{
+		[Test]
+		public void ConvertEmptyArrayShouldBeEmptyString ()
+		{
+			var bytes = new byte [0];
+			var result = HexConvert.ToString (bytes);
+			Assert.AreEqual (0, result.Length);
+		}
+
+		[Test]
+		public void ConvertOneByteShouldBeTwoCharacters ()
+		{
+			var bytes = new byte [] { 0x01 };
+			var result = HexConvert.ToString (bytes);
+			Assert.AreEqual (2, result.Length);
+			Assert.AreEqual ("01", result);
+		}
+
+		[Test]
+		public void ConvertFFShouldBeFF ()
+		{
+			var bytes = new byte [] { 0xff };
+			var result = HexConvert.ToString (bytes);
+			Assert.AreEqual (2, result.Length);
+			Assert.AreEqual ("ff", result);
+		}
+	}
+}
+
---
+++ CxSoftware.HexSharp/HexConvert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace CxSoftware.HexSharp
+{
+	public static class HexConvert
+	{
+		private static readonly char [] hexChars = "0123456789abcdef".ToCharArray ();
+
+		public static string ToString (byte [] bytes)
+		{
+			var sb = new StringBuilder (bytes.Length * 2);
+			foreach (var b in bytes)
+			{
+				sb.Append (hexChars [b / 0x10]);
+				sb.Append (hexChars [b % 0x10]);
+			}
+
+			return sb.ToString ();
+		}
+	}
+}
+
